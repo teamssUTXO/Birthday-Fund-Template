17f899878cdb0ca4428e4513e1fb05765bf01a89
fardellatimothe <timothe.fardella@viacesi.fr>
2025-06-20 13:25:53 +0200

    Goood

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,9 @@ import React, { useState } from 'react';
 import './Home.css';
 
 function Home() {
-  const [amount, setAmount] = useState('');
-
-  // Crée l'URL PayPal.me avec montant si montant > 0
-  const createPaypalLink = () => {
-    const baseLink = 'https://www.paypal.me/T9Noca';
-    const cleanAmount = parseFloat(amount);
-    if (cleanAmount > 0) {
-      // Limite à 2 décimales
-      return `${baseLink}/${cleanAmount.toFixed(2)}`;
-    }
-    return baseLink;
-  };
 
   const handleClick = () => {
-    const url = createPaypalLink();
-    window.open(url, '_blank');
+    window.open("https://www.paypal.com/donate/?hosted_button_id=DRARAU3BJVJ56", '_blank');
   };
 
   return (
@@ -28,7 +15,7 @@ function Home() {
       </div>
       <div className="columns">
         <div className="column-left">
-          <img src="https://fardellatimothe.github.io/cagnotte_maman/public/img/5d95c2c803819_airbus.jpg" alt="Voyage" />
+          <img src="img/5d95c2c803819_airbus.jpg" alt="Voyage" />
           <p>
             Pour son anniversaire, nous avons eu envie d’offrir à Brigitte un beau cadeau : un billet d’avion pour la Nouvelle-Calédonie (~2000 €).<br />
             Pour nous aider à réaliser cette surprise, j’ai mis en place une cagnotte en ligne pour celles et ceux qui préfèrent participer par virement.<br />
@@ -41,17 +28,17 @@ function Home() {
           <div className="contribution-box">
             <p><strong>Montant total de la cagnotte :</strong> 0€</p>
             <p><strong>Nombre de participants :</strong> 0</p>
-            <input
-              type="number"
-              placeholder="Montant en €"
-              min="1"
-              step="0.01"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
-            <button onClick={handleClick} disabled={!amount || parseFloat(amount) <= 0}>
+            
+            <div className="divider"></div>
+            
+            <button onClick={handleClick} >
               Contribuer
             </button>
+
+            <div className="divider"></div>
+
+            <p>Vous pouvez aussi participer en scannant le QR code ci-dessous :</p>
+            <img src='img/qr-code-paypal.png'></img>
           </div>
         </div>
       </div>
